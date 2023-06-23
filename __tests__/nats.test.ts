989e8e5924846fa0186a2b89a435f059962f705f
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2023-06-23 14:31:42 +0200

    added Tests

--- __tests__/nats.test.ts
+++ __tests__/nats.test.ts
@@ -2,6 +2,7 @@
 // setup.jest.js
 
 import { init } from '../lib';
+import { responseCallback } from '../lib/types/onMessageFunction';
 
 beforeAll(async () => {
   let server = {
@@ -11,6 +12,29 @@ beforeAll(async () => {
 
 describe('init', () => {
   let natsSpy: jest.SpyInstance;
+  beforeEach(() => {
+    // natsSpy = jest.spyOn(MockNatsjs,'connect');
+  });
 
-  beforeEach(() => {});
+  it('handleTransaction Should have been called', async () => {
+    
+    async function handleTransaction(reqObj: string, handleResponse: responseCallback): Promise<void> {
+      // Do stuff
+      const req = JSON.parse(reqObj);
+      req.Test = { some: 'val', another: 'one' };
+      const resp = JSON.stringify(req);
+      
+      // Done, so call response method
+      handleResponse(resp);
+    }
+    
+    const runServer = async (): Promise<void> => {
+      for (let retryCount = 0; retryCount < 10; retryCount++) {
+        if (!(await init(handleTransaction))) await new Promise((resolve) => setTimeout(resolve, 5000));
+        else break;
+      }
+    };
+
+    expect(handleTransaction).toBeDefined();
+  });
 });
