edc0e8dcf5d8fd8322fa68830a08e3c06779e35c
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2023-06-23 14:33:02 +0200

    fixed linting on tests

--- __tests__/nats.test.ts
+++ __tests__/nats.test.ts
@@ -17,17 +17,16 @@ describe('init', () => {
   });
 
   it('handleTransaction Should have been called', async () => {
-    
     async function handleTransaction(reqObj: string, handleResponse: responseCallback): Promise<void> {
       // Do stuff
       const req = JSON.parse(reqObj);
       req.Test = { some: 'val', another: 'one' };
       const resp = JSON.stringify(req);
-      
+
       // Done, so call response method
       handleResponse(resp);
     }
-    
+
     const runServer = async (): Promise<void> => {
       for (let retryCount = 0; retryCount < 10; retryCount++) {
         if (!(await init(handleTransaction))) await new Promise((resolve) => setTimeout(resolve, 5000));
