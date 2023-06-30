466ef04c43151d64a28e68f290cc282be5fb1114
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2023-06-30 12:33:00 +0200

    Fixed Overlapping Subject Error

--- src/services/jetstreamService.ts
+++ src/services/jetstreamService.ts
@@ -164,6 +164,12 @@ async function createStream(jsm: JetStreamManager, streamName: string, subjectNa
       if (subjectName) {
         logger.log(`Adding subject: ${subjectName} to stream: ${streamName}`);
         const streamInfo = await jsm.streams.info(stream);
+
+        if (streamInfo.config.subjects.includes(subjectName)){
+          logger.log("Subject Already present");
+          return;
+        }
+
         if (streamInfo.config.subjects) streamInfo.config.subjects.push(subjectName);
         else streamInfo.config.subjects = [subjectName];
         await jsm.streams.update(streamName, streamInfo.config);
