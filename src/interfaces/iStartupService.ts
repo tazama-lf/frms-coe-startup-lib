b77a0a06a57cb0c4abfe189c5f0af611e52bf808
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2023-07-07 11:24:17 +0200

    BUG: Added initProducer()

--- src/interfaces/iStartupService.ts
+++ src/interfaces/iStartupService.ts
@@ -5,5 +5,6 @@ import { type onMessageFunction } from '../types/onMessageFunction';
 
 export interface IStartupService {
   init: (onMessage: onMessageFunction, loggerService?: ILoggerService) => Promise<boolean>;
+  initProducer: (loggerService?: ILoggerService) => Promise<boolean>;
   handleResponse: (response: unknown, subject?: string[]) => Promise<void>;
 }
