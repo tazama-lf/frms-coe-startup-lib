d7c18fb66d7c8eec0eb2a419c7edc891e2c47d1e
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2025-04-10 11:27:38 +0200

    feat: added google cloud integration

--- src/interfaces/iRelayConfig.ts
+++ src/interfaces/iRelayConfig.ts
@@ -7,10 +7,14 @@ export interface IRelayConfig {
   destinationType: 'nats' | 'rabbitmq' | 'rest';
   destinationUrl: string;
   producerStream: string;
+  bucketName?: string;
+  googleApplicationCredentials?: string;
 }
 
 export const relayConfig: IRelayConfig = {
   destinationType: validateEnvVar('DESTINATION_TYPE', 'string'),
   destinationUrl: validateEnvVar('DESTINATION_URL', 'string'),
   producerStream: startupConfig.producerStreamName,
+  bucketName: validateEnvVar('GOOGLE_BUCKET_NAME', 'string', true),
+  googleApplicationCredentials: validateEnvVar('GOOGLE_APPLICATION_CREDENTIALS', 'string', true),
 };
